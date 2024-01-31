bc8fee72941340908f7cdf2c3794356d7b3c3c34
kim yujin <yujin3009@naver.com>
2024-01-31 20:15:31 +0900

    로그인 API 추가

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { prisma } from "../models/index.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+
 const router = express.Router();
 
 //회원가입 API
@@ -39,6 +42,19 @@ router.post("/sign-in", async (req, res, next) => {
 
   if (!user)
     return res.status(401).json({ message: "존재하지 않는 이메일입니다." });
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
+
+  const token = jwt.sign(
+    {
+      userId: user.userId,
+    },
+    "custom-secret-key"
+  );
+
+  // authotization 쿠키에 Berer 토큰 형식으로 JWT를 저장합니다.
+  res.cookie("authorization", `Bearer ${token}`);
+  return res.status(200).json({ message: "로그인 성공" });
 });
 
 export default router;
