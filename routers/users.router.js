16353bc07d4292a43e9bf826b3e69ff503d95c59
kim yujin <yujin3009@naver.com>
2024-01-31 19:35:28 +0900

    패키지 추가 설치

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { prisma } from "../models/index.js";
-
+import bcrypt from "bcrypt";
 const router = express.Router();
 
+//회원가입 API
 router.post("/sign-up", async (req, res, next) => {
   const { email, password, pwCheck, name } = req.body;
   const isExistUser = await prisma.users.findFirst({
@@ -23,11 +24,21 @@ router.post("/sign-up", async (req, res, next) => {
       .status(401)
       .json({ errorMessage: "비밀번호가 일치하지 않습니다." });
   }
+  const hashedPassword = await bcrypt.hash(password, 10);
   const user = await prisma.users.create({
-    data: { email, password, name },
+    data: { email, password: hashedPassword, name },
   });
 
   return res.status(201).json({ email, name });
+});
+
+// 로그인 API
+router.post("/sign-in", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await prisma.users.findFirst({ where: { email } });
+
+  if (!user)
+    return res.status(401).json({ message: "존재하지 않는 이메일입니다." });
 });
 
 export default router;
