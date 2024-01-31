2947aa6b59b5807665d96d10294c18e87877bd23
kim yujin <yujin3009@naver.com>
2024-02-01 01:56:27 +0900

    이력서 생성 api 추가

--- routers/documents.router.js
+++ routers/documents.router.js
@@ -1,7 +1,23 @@
 import express from "express";
 import { prisma } from "../models/index.js";
-import { Prisma } from "@prisma/client";
 
 const router = express.Router();
+
+// 이력서 생성 API
+router.post("/documents", async (req, res, next) => {
+  const { title, content } = req.body;
+  const { userId } = req.user;
+  if (!title)
+    return res.status(400).json({ errorMessage: "제목을 입력해주세요." });
+  if (!content)
+    return res.status(400).json({ errorMessage: "자기소개를 입력해주세요." });
+  const post = await prisma.posts.create({
+    data: {
+      userId: +userId,
+      title: title,
+      content: content,
+    },
+  });
+});
 
 export default router;
