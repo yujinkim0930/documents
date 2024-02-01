793b4d315528f2221130f96fee64045e88c3a8ee
kim yujin <yujin3009@naver.com>
2024-02-02 02:36:03 +0900

    이력서 조회 API 추가

--- routers/documents.router.js
+++ routers/documents.router.js
@@ -24,6 +24,48 @@ router.post("/documents", needSigninMiddlware, async (req, res, next) => {
   return res.status(201).json({ message: "이력서가 등록되었습니다." });
 });
 
+// 이력서 전체 목록 조회 API
+router.get("/documents", needSigninMiddlware, async (req, res, next) => {
+  const documents = await prisma.posts.findMany({
+    select: {
+      postId: true,
+      title: true,
+      content: true,
+      user: {
+        select: {
+          name: true,
+        },
+      },
+      status: true,
+      createdAt: true,
+    },
+  });
+  return res.status(200).json({ documents });
+});
+
+// 이력서 상세 조회 API
+router.get("/documents/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+  if (!postId)
+    return res.status(401).json({ message: "이력서 조회에 실패하였습니다." });
+  const post = await prisma.posts.findFirst({
+    where: { postId: +postId },
+    select: {
+      postId: true,
+      title: true,
+      content: true,
+      user: {
+        select: {
+          name: true,
+        },
+      },
+      createdAt: true,
+    },
+  });
+
+  return res.status(200).json({ data: post });
+});
+
 // 이력서 수정 API
 router.put(
   "/documents/:postId",
